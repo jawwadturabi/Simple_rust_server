4c3dedfb9efe28c63efedb18761e1be95cfa0583
jawwadturabi <memonjawwad9@gmail.com>
2020-03-08 05:37:44 +0500

    Server is running

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+use simple_server::{Server, StatusCode};
+
+fn main() {
+    let server = Server::new(|req, mut response| {
+        let body = String::from_utf8(req.body().to_vec()).unwrap();
+        println!("Request received {} {} {:?}", req.method(), req.uri(), body);
+
+        match (req.method().as_str(), req.uri().path()) {
+            ("GET", "/hello") => Ok(response.body(String::from("hello_Rust").into_bytes())?),
+
+            ("POST", "/hello") => Ok(response.body(body.into_bytes())?),
+
+            (_, _) => {
+                response.status(StatusCode::NOT_FOUND);
+                Ok(response.body(String::from("Not Found").into_bytes())?)
+            }
+        }
+    });
+
+    server.listen("127.0.0.1", "4000");
+}
